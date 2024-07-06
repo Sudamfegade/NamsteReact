b3fb45341a22b2a9971bace6a5f7037ef359f26a
Sudam Fegade <sudamfegadepbn@gmail.com>
2024-07-06 15:03:06 +0530

    igniting app.

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
+  React.createElement("div", { id: "child", key: "sdf" }, [
     React.createElement("h1", {}, "I'm H1 tag"),
     React.createElement("h2", {}, "I'm H2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
+  React.createElement("div", { id: "child2", key: "asdfd" }, [
     React.createElement("h1", {}, "I'm H1 tag"),
     React.createElement("h2", {}, "I'm H2 tag"),
   ]),
