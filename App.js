4073ad85dec54547ddf4ffb84b23c6e1657f1bc3
Sudam Fegade <sudamfegadepbn@gmail.com>
2024-07-06 23:31:56 +0530

    ep03

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child", key: "sdf" }, [
-    React.createElement("h1", {}, "I'm H1 tag"),
-    React.createElement("h2", {}, "I'm H2 tag"),
-  ]),
-  React.createElement("div", { id: "child2", key: "asdfd" }, [
-    React.createElement("h1", {}, "I'm H1 tag"),
-    React.createElement("h2", {}, "I'm H2 tag"),
-  ]),
-]);
+const Heading = () => (
+  <h1 className="head" tabIndex={0}>
+    Namste react
+  </h1>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Heading />);
